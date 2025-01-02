2be2d191b36871e4754be4a094dca63994f43796
SummySumanth <sumanth261094@gmail.com>
2025-01-03 01:10:03 +0530

    Remove secret access logic from Environment function for production

--- src/utils/secrets.js
+++ src/utils/secrets.js
@@ -1,18 +1,7 @@
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-
-const client = new SecretManagerServiceClient();
-
-async function accessSecret(keyName) {
-  const [version] = await client.accessSecretVersion({
-    name: `projects/392632175768/secrets/${keyName}/versions/latest`,
-  });
-  return version.payload.data.toString('utf8');
-}
-
 function Environment(NODE_ENV) {
   if (process.env.NODE_ENV === 'production') {
-    const mediumToken = accessSecret('medium-api-token');
-    const hashnodeToken = accessSecret('hashnode-access-token');
+    const mediumToken = "";
+    const hashnodeToken = "";
     this.keysAndValues = {
       mediumToken,
       hashnodeToken,
