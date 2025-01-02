6bbb6b57d4d09dd95ed9b7cfaf061bb3d5a9ab76
SummySumanth <sumanth261094@gmail.com>
2025-01-03 01:52:55 +0530

    Comment out secret access logic in secrets.js for production readiness

--- src/utils/secrets.js
+++ src/utils/secrets.js
@@ -1,18 +1,20 @@
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+// const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
-const client = new SecretManagerServiceClient();
+// const client = new SecretManagerServiceClient();
 
-async function accessSecret(keyName) {
-  const [version] = await client.accessSecretVersion({
-    name: `projects/392632175768/secrets/${keyName}/versions/latest`,
-  });
-  return version.payload.data.toString('utf8');
-}
+// async function accessSecret(keyName) {
+//   const [version] = await client.accessSecretVersion({
+//     name: `projects/392632175768/secrets/${keyName}/versions/latest`,
+//   });
+//   return version.payload.data.toString('utf8');
+// }
 
 function Environment(NODE_ENV) {
   if (process.env.NODE_ENV === 'production') {
     const mediumToken = accessSecret('medium-api-token');
     const hashnodeToken = accessSecret('hashnode-access-token');
+    const mediumToken = accessSecret('');
+    const hashnodeToken = accessSecret('');
     this.keysAndValues = {
       mediumToken,
       hashnodeToken,
