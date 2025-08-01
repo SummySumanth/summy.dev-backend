e279a96075a7ed562d70374af598a30335c2354d
SummySumanth <sumanth261094@gmail.com>
2025-08-01 19:01:08 +0530

    Refactor downloader logic to use updated file paths and improve code formatting; add new resume PDF file.

--- src/controllers/downloaders/downloaders.js
+++ src/controllers/downloaders/downloaders.js
@@ -1,31 +1,39 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const downloader = (req, res) => {
   let src;
   switch (req.params.filename) {
-    case 'resume':
-      src = fs.createReadStream(path.join(__dirname, '../../storage/pdf/sumanth_react_dev_resume_2024.pdf'));
+    case "resume":
+      src = fs.createReadStream(
+        path.join(
+          __dirname,
+          "../../storage/pdf/Sumanth-Frontend-React-Developer-8_Years_Exp.pdf"
+        )
+      );
       res.writeHead(200, {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': 'attachment; filename=sumanth-react-dev-resume.pdf',
-        'Content-Transfer-Encoding': 'Binary',
+        "Content-Type": "application/pdf",
+        "Content-Disposition":
+          "attachment; filename=sumanth-react-dev-resume.pdf",
+        "Content-Transfer-Encoding": "Binary",
       });
 
       src.pipe(res);
       break;
-    case 'vcard':
-      src = fs.createReadStream(path.join(__dirname, '../../storage/vcard/vcard.vcf'));
+    case "vcard":
+      src = fs.createReadStream(
+        path.join(__dirname, "../../storage/vcard/vcard.vcf")
+      );
       res.writeHead(200, {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': 'attachment; filename=vcard.vcf',
-        'Content-Transfer-Encoding': 'Binary',
+        "Content-Type": "application/pdf",
+        "Content-Disposition": "attachment; filename=vcard.vcf",
+        "Content-Transfer-Encoding": "Binary",
       });
 
       src.pipe(res);
       break;
     default:
-      res.status(404).send('Requested file not found');
+      res.status(404).send("Requested file not found");
       break;
   }
 };
