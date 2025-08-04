7b72a5cde3b63a47e46e0fa737475d97808ab0b6
SummySumanth <sumanth261094@gmail.com>
2025-08-04 12:01:15 +0530

    Update downloader to use new resume PDF file and add the latest version of the frontend developer resume.

--- src/controllers/downloaders/downloaders.js
+++ src/controllers/downloaders/downloaders.js
@@ -8,7 +8,7 @@ const downloader = (req, res) => {
       src = fs.createReadStream(
         path.join(
           __dirname,
-          "../../storage/pdf/Sumanth-Frontend-React-Developer-8_Years_Exp_v2.pdf"
+          "../../storage/pdf/Sumanth-Frontend-React-Developer-8_Years_Exp_v3.pdf"
         )
       );
       res.writeHead(200, {
